5f928163402923e4157ec60d48693e5e744e8eb2
sanya <backup.sanya@gmail.com>
2020-09-14 11:31:38 +0300

    Adding JPA

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.example.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			repository.save(new Book("To Kill A Mockingbird", "Harper Lee", 2010, 1001, 150));
+			repository.save(new Book("Great Expectations", "Charles Dickens", 2000, 1002, 200));
+
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
+
+	}
 }
