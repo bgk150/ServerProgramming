43db9fab870f51c9674cdeb7f934f81f40b5a51e
sanya <backup.sanya@gmail.com>
2020-10-12 12:41:00 +0300

    Corrected delete option

--- src/main/java/com/example/Bookstore/web/BookstoreController.java
+++ src/main/java/com/example/Bookstore/web/BookstoreController.java
@@ -46,7 +46,7 @@ public class BookstoreController {
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	@PreAuthorize("hasRole('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
