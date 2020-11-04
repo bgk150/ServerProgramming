29b0d16920914ecc076d937167c3e56f7fe7b591
sanya <backup.sanya@gmail.com>
2020-11-04 20:23:37 +0200

    unique username

--- src/main/java/com/example/Bookstore/domain/Customer.java
+++ src/main/java/com/example/Bookstore/domain/Customer.java
@@ -15,7 +15,7 @@ public class Customer {
     private Long id;
 
     // Username with unique constraint
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
     @Column(name = "password", nullable = false)
